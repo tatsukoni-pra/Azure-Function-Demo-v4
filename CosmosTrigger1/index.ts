3f80e49d840f4f131135faa5b3deb05c66d58eb6
小西達大 <konishitatsuhiro@konishitoorudainoMacBook-Pro.local>
2023-12-04 21:56:46 +0900

    fix: コード調整

--- CosmosTrigger1/index.ts
+++ CosmosTrigger1/index.ts
@@ -3,43 +3,17 @@ import { AzureFunction, Context } from "@azure/functions"
 const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
 process.on('SIGTERM', () => {
-    console.log('処理が途中終了されました。');
-    // クリーンアップ処理
-    process.exit(0);
-});
-
-process.on('SIGINT', () => {
-    console.log('処理が中断されました。');
-    // クリーンアップ処理
-    process.exit(0);
-});
-
-process.on('SIGHUP', () => {
-    console.log('処理が中断されました。');
-    // クリーンアップ処理
-    process.exit(0);
-});
-
-process.on('exit', () => {
-    console.log('処理がexitされました。');
-    // クリーンアップ処理
-    process.exit(0);
-});
-
-process.on('uncaughtException', () => {
-    console.log('処理が異常終了されました。');
-    // クリーンアップ処理
+    console.log('Function SIGTERM');
     process.exit(0);
 });
 
 const cosmosDBTrigger: AzureFunction = async function (context: Context, documents: any[]): Promise<void> {
-    console.log('実行開始');
+    console.log('Function Start');
 
     if (!!documents && documents.length > 0) {
         context.log('Document Id: ', documents[0].id);
     }
 
-    // 3分30秒待機
     await sleep(30000);
     context.log('30秒経過...');
     await sleep(60000);
@@ -49,8 +23,7 @@ const cosmosDBTrigger: AzureFunction = async function (context: Context, documen
     await sleep(60000);
     context.log('3分30秒経過');
 
-    // 完了
-    context.log('Finished!!!');
+    console.log('Function Finished');
 }
 
 export default cosmosDBTrigger;
--- HttpTrigger1/index.ts
+++ HttpTrigger1/index.ts
@@ -3,22 +3,12 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
 process.on('SIGTERM', () => {
-    console.log('SIGTERM');
-    process.exit(0);
-});
-
-process.on('SIGINT', () => {
-    console.log('SIGINT');
-    process.exit(0);
-});
-
-process.on('SIGHUP', () => {
-    console.log('SIGHUP');
+    console.log('Function SIGTERM');
     process.exit(0);
 });
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    console.log('実行開始');
+    console.log('Function Start');
 
     await sleep(30000);
     context.log('30s...');
@@ -29,7 +19,7 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     await sleep(60000);
     context.log('210s...');
 
-    context.log('Finished!!!');
+    console.log('Function Finished');
 
     context.res = {
         body: "Hello, World!"
