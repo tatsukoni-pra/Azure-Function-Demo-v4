eebc19a4a45a7f3cdd850a286b1332b11d149c4e
小西達大 <konishitatsuhiro@konishitoorudainoMacBook-Pro.local>
2024-01-23 00:28:42 +0900

    feat: Service Bus考慮

--- CosmosTrigger1/index.ts
+++ CosmosTrigger1/index.ts
@@ -1,18 +1,44 @@
 import { AzureFunction, Context } from "@azure/functions"
+import { ServiceBusClient } from "@azure/service-bus";
 
-const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
+const connectionString = process.env.SERVICE_BUS_CONNECTION_STRING;
+const queueName = "mytopic";
 
-process.on('SIGTERM', () => {
-    console.log('Function SIGTERM');
-    process.exit(0);
-});
+const messageId = () => {
+    return crypto.randomUUID() + '--' + new Date().getTime();
+}
 
 const cosmosDBTrigger: AzureFunction = async function (context: Context, documents: any[]): Promise<void> {
     console.log('Function Start');
 
-    if (!!documents && documents.length > 0) {
-        context.log('Document Id: ', documents[0].id);
-        context.log(documents);
+    const sbClient = new ServiceBusClient(connectionString);
+    const sender = sbClient.createSender(queueName);
+
+    try {
+        const message = {
+            body: {
+                "type": "Albert Einstein",
+                "content": {
+                    "user_id": documents[0].id,
+                    "text": documents[0].text,
+                }
+            },
+            messageId: messageId()
+        };
+        let batch = await sender.createMessageBatch();
+        if (!batch.tryAddMessage(message)) {
+            await sender.sendMessages(batch);
+            batch = await sender.createMessageBatch();
+            if (!batch.tryAddMessage(message)) {
+                throw new Error("Message too big to fit in a batch");
+            }
+        }
+        await sender.sendMessages(batch);
+
+        console.log(`Sent a batch of messages to the queue: ${queueName}`);
+        await sender.close();
+    } finally {
+        await sbClient.close();
     }
 
     console.log('Function Finished');
